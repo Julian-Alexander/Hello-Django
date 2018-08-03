a17c1a8e8cd644f5182fc61cd4e1b79e14cb9514
JulianAlexander <julian.pineyro@gmail.com>
2018-08-03 21:09:01 +0100

    Database Fix

--- djorg/settings.py
+++ djorg/settings.py
@@ -84,10 +84,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'URL': dj_database_url.config('DATABASE_URL', conn_max_age=600)
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
